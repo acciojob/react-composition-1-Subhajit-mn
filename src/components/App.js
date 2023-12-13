7b6fd7dbf898bf2a09a6fcc0b264aa8f8da268c8
Subhajit_Mandal <subhajitmandal11@gmail.com>
2023-12-13 19:16:03 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@
 import React from "react";
 import './../styles/App.css';
 
+const tabsData = [
+  {titles: "Tab 1", contents: "This is the content for Tab 1."},
+  {titles: "Tab 1", contents: "This is the content for Tab 2."},
+  {titles: "Tab 1", contents: "This is the content for Tab 3."},
+];
+
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Tabs tabs={tabs} />
+
     </div>
   )
 }
